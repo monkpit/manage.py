4f96ad6e2b3c8ddfe98b4d59b5504b5bb13316ea
jean-philippe serafin <serafinjp@gmail.com>
2015-01-29 07:41:38 +0000

    Bump 0.2.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manage.py',
-    version='0.2.8',
+    version='0.2.9',
     description='Human friendly CLI builder',
     long_description='',
     author='jean-philippe serafin',
